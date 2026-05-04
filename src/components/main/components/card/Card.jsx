34f6cc4704442078820b84f6136c795ee3820245
Maisa Santos de Lana <msantos@eslsistemas.com.br>
2026-05-04 19:02:07 -0300

    Fetch likes from the server

--- src/components/main/components/card/Card.jsx
+++ src/components/main/components/card/Card.jsx
@@ -1,21 +1,21 @@
-export default function Card(props){
-    const {name, link, isLiked} = props.card;
-    return(
-        
-            <div className="gallery__item">
-                <span className="delete material-symbols-rounded"> delete </span>
-                <img 
-                className="gallery__image" 
-                src={link} alt={name}
-                onClick={() => props.onCardClick(props.card)
-
-                } />
-                <div className="gallery__image-item">
-                    <span className="gallery__image-name">{name}</span>
-                    <span className="gallery__image-like material-symbols-outlined">favorite</span>
-                </div>
-                
-            </div>
-      
-    )
+export default function Card(props) {
+  const { name, link, isLiked } = props.card;
+  const cardLikeButtonClassName = `gallery__image-like material-symbols-outlined  ${
+    isLiked ? "material-symbols-rounded" : ""
+  }`;
+  return (
+    <div className="gallery__item">
+      <span className="delete material-symbols-rounded"> delete </span>
+      <img
+        className="gallery__image"
+        src={link}
+        alt={name}
+        onClick={() => props.onCardClick(props.card)}
+      />
+      <div className="gallery__image-item">
+        <span className="gallery__image-name">{name}</span>
+        <span className={cardLikeButtonClassName}>favorite</span>
+      </div>
+    </div>
+  );
 }
